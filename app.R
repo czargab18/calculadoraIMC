99e21a7517df127d7f0b4d5bc3667c4a65592cac
czargab18 <cesargabrielcode@gmail.com>
2023-12-01 19:45:21 -0300

    style: style

--- app.R
+++ app.R
@@ -12,50 +12,63 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   tags$head(
-    tags$link(rel = "stylesheet", type = "styles/css", href = "style.css")
+    tags$title("calculadora de IMC"),
+    tags$link(
+      rel = "stylesheet", type = "text/css",
+      href = "styles/style.css"
+    ),
   ),
-  div(id="content",
-  h1("Calculadora de IMC"),
-  div(id = "content-input",
-      numericInput(inputId = "input-text-peso",
-                   label = "Qual o seu peso em kilogramas?",
-                   value = 60)
-  ),
-  div(id = "content-input",
-      numericInput(inputId = "input-text-altura",
-                   label = "Qual o seu peso em centimetros?",
-                   value = 173)
-  ),
-  span(id = "linha-separacao"),
-  div(id="content-resultado",
-      textOutput(outputId = "resultado",
-                 )),
-  div(id="content-resultado-ideal",
-      textOutput(outputId = "resultado-ideal",
-      ))
+  includeCSS("styles/style.css"),
+
+  div(
+    id = "content",
+    h1("Calculadora de IMC"),
+    div(
+      id = "content-input",
+      numericInput(
+        inputId = "input-text-peso",
+        label = "Qual o seu peso em kilogramas?",
+        value = 60
+      )
+    ),
+    div(
+      id = "content-input",
+      numericInput(
+        inputId = "input-text-altura",
+        label = "Qual o seu peso em centimetros?",
+        value = 173
+      )
+    ),
+    span(id = "linha-separacao"),
+    div(
+      id = "content-resultado",
+      textOutput(outputId = "resultado", )
+    ),
+    div(
+      id = "content-resultado-ideal",
+      textOutput(outputId = "resultado-ideal", )
+    )
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  imc <- reactive(
-    {input$'input-text-peso'/((input$'input-text-altura'/100)^2)}
-  )
+  imc <- reactive({
+    input$"input-text-peso" / ((input$"input-text-altura" / 100)^2)
+  })
   output$resultado <- renderText({
-    paste0("Seu Indice de Massa Corporal é: ",round(imc(),2))
- 
-   })
-  output$'resultado-ideal' <- renderText({
-    if(imc()<18.5){
+    paste0("Seu Indice de Massa Corporal é: ", round(imc(), 2))
+  })
+  output$"resultado-ideal" <- renderText({
+    if (imc() < 18.5) {
       "Você está abaixo do peso!"
-    } else if(imc()<25){
-      'Você está dentro do peso ideal!'
-    } else{
-      'Você está acima do peso!'
+    } else if (imc() < 25) {
+      "Você está dentro do peso ideal!"
+    } else {
+      "Você está acima do peso!"
     }
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
